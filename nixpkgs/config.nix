773bcbda2a3dc6496b7dff6600c59bb7b793a70f
Antonio Yang <yanganto@gmail.com>
2020-11-16 23:34:31 +0800

    add neovim v0.5.0 (nightly)

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,10 +1,18 @@
 {
   packageOverrides = pkgs: rec {
-    nix-folder2channel = pkgs.callPackage ./nix-folder2channel {};
-    nvidia-xrun = pkgs.callPackage ./nvidia-xrun {
-      inherit (pkgs.linuxPackages) nvidia_x11 bbswitch;
-      inherit (pkgs.xorg) xinit xrandr;
+    nix-folder2channel = pkgs.callPackage ./nix-folder2channel { };
+    # nvidia-xrun = pkgs.callPackage ./nvidia-xrun {
+    #   inherit (pkgs.linuxPackages) nvidia_x11 bbswitch;
+    #   inherit (pkgs.xorg) xinit xrandr;
+    # };
+
+    wrapNeovimUnstable = pkgs.callPackage ./neovim/wrapper.nix { };
+    wrapNeovim = neovim-unwrapped: pkgs.makeOverridable (neovimUtils.legacyWrapper neovim-unwrapped);
+    neovim-unwrapped = pkgs.callPackage ./neovim {
+      lua = pkgs.lua5_1;
     };
+    neovimUtils = pkgs.callPackage ./neovim/utils.nix { };
+    neovim = pkgs.wrapNeovim neovim-unwrapped { };
 
     # Some note for building Rust packages
     # rust_1_46 = pkgs.callPackage ./rust_146.nix {
@@ -19,4 +27,3 @@
   allowBroken = true;
   allowUnfree = true;
 }
-
