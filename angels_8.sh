ddcaea57d19d22b473c1dae75e011826dd1bf8f8
Antonio Yang <yanganto@gmail.com>
2021-05-04 10:31:12 +0800

    update gitui for MacOS

--- angels_8.sh
+++ angels_8.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env nu
 
 let hostname = $(sys | get host.name)
+let nixversion = $(nix-env --version)
 
 # Build up command line developing environment
 build-string "===> Angels  8 - gitui, gitconfig, developing hooks " $(char nl)
@@ -8,9 +9,13 @@ build-string "===> Angels  8 - gitui, gitconfig, developing hooks " $(char nl)
 # setup config for gitconfig
 if $hostname != "Windows" {cp gitconfig ~/.gitconfig } {}
 
-# setup key config for gitui
-if $hostname == "Linux" {cp key_config.ron ~/.config/gitui/ } {}
-if $hostname == "Darwin" {cp key_config.ron "~/Library/Application Support/gitui/key_config.ron"} {}
+# setup key config for gitui and install gitui
+if $hostname == "Windows" || {cargo install gitui} {}
+if $nixversion =~ "nix-env" && $hostname == "NixOS" {nix-env -iA nixos.gitAndTools.gitui} {}
+if $nixversion =~ "nix-env" && $hostname != "NixOS" {nix-env -iA nixpkgs.gitAndTools.gitui} {}
+
+if $hostname != "Windows" {mkdir ~/.config/gitui/ } {}
+if $hostname != "Windows" {cp key_config.ron ~/.config/gitui/ } {}
 
 # update default git hooks
 if $hostname != "Windows" {cp -r direnv ~/.config/} {}
