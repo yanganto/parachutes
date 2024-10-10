408f223e0b805566ff3192f4fc970541e3b4db58
Antonio Yang <yanganto@gmail.com>
2024-10-10 09:04:33 +0800

    rm download script

--- take_off.sh
+++ take_off.sh
@@ -4,6 +4,7 @@ echo "===> Take Off - download and init repos"
 if ! [[ -f angels_10.sh ]]; then
     # if there is only take off script check out this repo, get the reminding scripts
     git clone https://github.com/yanganto/parachutes.git --depth=1
+    rm -rf parachute_take_off
     cd parachutes
 fi
 
