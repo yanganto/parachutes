a23908ebb9c4f3fbf97e58a8d6f3ab4e4ac2c5f0
Antonio Yang <yanganto@gmail.com>
2026-09-26 15:09:20 +0800

    refact devshells

--- nix/shells/middle.nix
+++ nix/shells/middle.nix
@@ -1,28 +1,22 @@
 {
   lib,
   pkgs,
-  PROMPT ? "",
   ...
 }:
 let
   enable_ap = pkgs.writeShellScriptBin "enable_ap" (lib.readFile ../scripts/enable_ap.sh);
   dump = pkgs.writeShellScriptBin "dump" (lib.readFile ../scripts/dump.sh);
 in
-pkgs.mkShell {
+pkgs.myMkShell {
   name = "middle";
-  shellHook = ''
-    export DEVSHELL=middle
-    ${PROMPT}
-  '';
-
+  myScripts = [
+    enable_ap
+    dump
+  ];
   nativeBuildInputs = with pkgs; [
     iw
     hostapd
     dnsmasq
     tcpdump
-
-    # Scripts
-    enable_ap
-    dump
   ];
 }
